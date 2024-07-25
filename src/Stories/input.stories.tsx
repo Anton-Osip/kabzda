39890dc86ffae90a6d60112489aae90ceccbde0e
Anton-Osip <osipchika@gmail.com>
2024-07-25 13:06:41 +0300

    13 - controlled input, checkbox, select

--- src/Stories/input.stories.tsx
+++ src/Stories/input.stories.tsx
@@ -34,3 +34,30 @@ export const GetValueOfUncontrolledInputByButtonPress = () => {
         </>)
 }
 
+export const ControlledInput = () => {
+    const [value, setValue] = useState('')
+    return <><input value = {value} onChange = {(e) => {
+        setValue(e.currentTarget.value)
+    }}/>--{value}</>
+}
+export const ControlledCheckbox = () => {
+    const [value, setValue] = useState(true)
+    return <> <input type = {'checkbox'} checked = {value} onChange = {(e) => {
+        setValue(e.currentTarget.checked)
+    }}/>--{value.toString()}</>
+}
+export const ControlledSelect = () => {
+    const [value, setValue] = useState<string>(2)
+    return (
+        <>
+            <select value = {value} onChange = {(e) => {
+                setValue(e.currentTarget.value)
+            }}>
+                <option>none</option>
+                <option value = {1}>Select2</option>
+                <option value = {2}>Select3</option>
+                <option value = {3}>Select4</option>
+            </select>
+            -- {value.toString()}
+        </>)
+}
